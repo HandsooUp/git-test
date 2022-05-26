50a8a6f246382663a6fbd990aa641c0502d02741
HandsUp <1393059127@qq.com>
2022-05-26 21:02:21 +0800

    first commit

--- src/main/java/com/jj/git/GitTest.java
+++ src/main/java/com/jj/git/GitTest.java
@@ -1,7 +1,10 @@
 package com.jj.git;
 
 public class GitTest {
-    public static void main(String[] args) {
+    public static void main1(String[] args) {
         System.out.println("hello Git！");
+        System.out.println("hello Git2！");
     }
+
+
 }
