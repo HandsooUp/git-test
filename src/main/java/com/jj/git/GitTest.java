4337cea5c88f87e7acce66715483444c56a78d72
HandsooUp <106309541+HandsooUp@users.noreply.github.com>
2022-05-26 21:27:33 +0800

    Update GitTest.java

--- src/main/java/com/jj/git/GitTest.java
+++ src/main/java/com/jj/git/GitTest.java
@@ -7,6 +7,7 @@ public class GitTest {
         System.out.println("hello Git3！");
         System.out.println("hello Git4！");
         System.out.println("hello Git4！push");
+        System.out.println("hello Git4！poll");
     }
 
 
